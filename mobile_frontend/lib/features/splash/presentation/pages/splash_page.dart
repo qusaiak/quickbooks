86e99c0d2d8d6c51c923433b27599403fca1b962
qusai <qusai2000abokhier@gmail.com>
2025-06-14 14:04:12 +0300

    create splash screen

---
+++ mobile_frontend/lib/features/splash/presentation/pages/splash_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../widgets/splash_body.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashBody(),
+    );
+  }
+}
